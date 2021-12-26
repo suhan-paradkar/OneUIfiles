1d28d1d95c4ca95d26ea5a33709b7d7949ac2e92
Wisest_wizard <12suhangp34@gmail.com>
2021-12-26 11:36:48 +0530

    Revert "Squash bugs"
    This reverts commit fd21b16817d0fda1cd3913e6154a9db21d027574.

--- app/src/main/java/me/zhanghai/android/files/about/AboutActivity.kt
+++ app/src/main/java/me/zhanghai/android/files/about/AboutActivity.kt
@@ -7,12 +7,18 @@ package me.zhanghai.android.files.about
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import me.zhanghai.android.files.app.AppActivity
 
 class AboutActivity : AppActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Calls ensureSubDecor().
         findViewById<View>(android.R.id.content)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit { add<AboutFragment>(android.R.id.content) }
+        }
     }
 }
